2ad35b75bb5a194fc97025b36bc070c0ea519dad
stareu <exstarence@gmail.com>
2025-04-02 11:19:47 +0300

    upd: animejs

--- js/App.js
+++ js/App.js
@@ -1,4 +1,5 @@
 import { Application, Sprite, Assets, Texture, Container, Spritesheet, Graphics } from 'pixi.js'
+import anime from 'animejs'
 import { Layout } from '@pixi/layout'
 
 const APP_WIDTH =  1366
@@ -36,6 +37,22 @@ class App {
 				height: innerHeight
 			})
 
+			const gameArea = this.myCont.getChildByID('gameArea')
+
+			let gameScale
+			const heightScale = innerHeight / APP_HEIGHT
+
+			if (innerWidth < APP_WIDTH * heightScale) {
+				gameScale = innerWidth / APP_WIDTH
+			}
+			else {
+				gameScale = heightScale
+			}
+
+			gameArea.setStyles({
+				scale: gameScale
+			})
+
 			this.myCont.resize(innerWidth, innerHeight)	
 			// this.myCont.refresh()
 		}
@@ -51,36 +68,51 @@ class App {
 			'./img/sky.jpg'
 		])
 
-		const sprite = Sprite.from('./img/home1.jpg')
-		const sprite2 = Sprite.from('./img/sky.jpg')
+		const sprite2 = Sprite.from('./img/home1.jpg')
+		const sprite = Sprite.from('./img/sky.jpg')
 		const gameArea = new Graphics()
 			.rect(0, 0, 400, 400)
 			.fill('rgba(255,255,255,.5)')
+
+		// const animation = anime({
+		// 	targets: sprite,
+		// 	x: 500,
+		// 	direction: 'alternate',
+		// 	easing: 'easeInOutQuad',
+		// 	loop: true,
+		// 	duration: 1e3,
+		// 	autoplay: false
+		// })
+
+		// this.pixiApp.ticker.add(ticker => {
+		// 	animation.tick(ticker.lastTime)
+		// })
 
 		const mycont = this.myCont = new Layout({
 			content: {
 				gameArea: {
 					content: {
+						mySprite2: {
+							content: sprite2,
+							styles: {
+								maxWidth: 1920,
+								maxHeight: 1080,
+							}
+						},
 						mySprite: {
 							content: sprite,
 							styles: {
 								maxHeight: 200,
-								position: 'rightBottom',
-								// anchor: 0.5
-							}
-						},
-						mySprite2: {
-							content: sprite2,
-							styles: {
-								maxHeight: 200,
+								position: 'leftBottom',
 							}
 						}
 					},
 					styles: {
 						position: 'center',
-						width: 400,
-						height: 400,
-						background: gameArea
+						width: APP_WIDTH,
+						height: APP_HEIGHT,
+						background: gameArea,
+						overflow: 'hidden'
 					}
 				},
 			}
